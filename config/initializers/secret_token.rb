1a192b4ed5c4e04c3118fb72f1441994d3cd9880
Mathias Kaufmann <me@stei.gr>
2013-01-21 10:40:45 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WorksheetGenerator::Application.config.secret_token = 'c3979f31ebdf385b69f9526972c18aa208c713e76327f7aca5f7eeabe8f7234448108b978f62ce698ccd5cea80c77381471ddb4020f67caf95680688ab295829'
