bdfbe45d559db17cf8f7d0f10887ddcb50fb3cdd
Mathias Kaufmann <me@stei.gr>
2013-01-21 13:57:51 +0100

    Simple syntax-fix

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module WorksheetGenerator
     # don't generate RSpec tests for views and helpers
     config.generators do |g|
       
-      g.test_framework :rspec, fixture: true
+      g.test_framework :rspec, :fixture => true
       g.fixture_replacement :factory_girl
       
       
